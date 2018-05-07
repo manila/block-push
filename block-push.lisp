c7cc44da9b932d002695fc2489c2005189320004
manila <manila@users.noreply.github.com>
2018-05-06 19:05:09 -0600

    Added collision detection and sprite scaling

--- block-push.lisp
+++ block-push.lisp
@@ -5,66 +5,87 @@
 
 (defvar *canvas-width* 576)
 (defvar *canvas-height* 576)
-(defparameter *w1* (gamekit:vec2 0 896))
-(defparameter *w2* (gamekit:vec2 64 896))
-(defparameter *w3* (gamekit:vec2 128 896))
-(defparameter *f1* (gamekit:vec2 0 832))
-(defparameter *f2* (gamekit:vec2 64 832))
-(defparameter *b1* (gamekit:vec2 0 960))
-(defparameter *b2* (gamekit:vec2 64 960))
-(defparameter *player-sprite* (gamekit:vec2 0 768))
-(defvar *player-x* 2)
-(defvar *player-y* 2)
-(defvar *block-map* `(
-  ,(list *w1* *w2* *w3* *w1* 'nil *w3* *w1* *w2* *w3*)
-  ,(list *w1* 'nil 'nil 'nil 'nil 'nil 'nil 'nil *w3*)
-  ,(list *w1* 'nil *b1* *b2* *b1* *b1* *b1* 'nil *w3*)
-  ,(list *w1* 'nil *b1* 'nil 'nil 'nil *b1* 'nil *w3*)
-  ,(list *w1* 'nil *b1* 'nil *b1* 'nil *b1* 'nil *w3*)
-  ,(list *w1* 'nil *b1* 'nil 'nil 'nil *f1* 'nil *w3*)
-  ,(list *w1* 'nil *b1* *b1* *b2* *b1* *b2* 'nil *w3*)
-  ,(list *w1* 'nil 'nil 'nil 'nil 'nil 'nil 'nil *w3*)
-  ,(list *w1* *w2* *w3* *w1* *w2* *w3* *w1* *w2* *w3*)))
+(defparameter *w1* (gamekit:vec2 0 224))
+(defparameter *w2* (gamekit:vec2 16 224))
+(defparameter *w3* (gamekit:vec2 32 224))
+(defparameter *f1* (gamekit:vec2 0 208))
+(defparameter *f2* (gamekit:vec2 16 208))
+(defparameter *b1* (gamekit:vec2 0 240))
+(defparameter *b2* (gamekit:vec2 16 240))
+(defparameter *player-sprite* (gamekit:vec2 0 192))
+(defvar *player-x* 4)
+(defvar *player-y* 3)
+(setf *block-map* (make-array '(9 9)
+  :initial-contents `(
+  (,*w1* ,*w2* ,*w3* ,*w1* ,'nil ,*w3* ,*w1* ,*w2* ,*w3*)
+  (,*w1* ,'nil ,'nil ,'nil ,'nil ,'nil ,'nil ,'nil ,*w3*)
+  (,*w1* ,'nil ,*b1* ,*b2* ,*b1* ,*b1* ,*b1* ,'nil ,*w3*)
+  (,*w1* ,'nil ,*b1* ,'nil ,'nil ,'nil ,*f1* ,'nil ,*w3*)
+  (,*w1* ,'nil ,*b1* ,'nil ,*b1* ,'nil ,*b1* ,'nil ,*w3*)
+  (,*w1* ,'nil ,*b1* ,'nil ,'nil ,'nil ,*b1* ,'nil ,*w3*)
+  (,*w1* ,'nil ,*b1* ,*b1* ,*b1* ,*b1* ,*b2* ,'nil ,*w3*)
+  (,*w1* ,'nil ,'nil ,'nil ,'nil ,'nil ,'nil ,'nil ,*w3*)
+  (,*w1* ,*w2* ,*w3* ,*w1* ,*w2* ,*w3* ,*w1* ,*w2* ,*w3*)))
+)
 
 (gamekit:defgame block-push () ()
   (:viewport-width *canvas-width*)
   (:viewport-height *canvas-height*)
   (:viewport-title "Block Push"))
 
-(gamekit:bind-button :mouse-left :released
-  (lambda () (setf *player-x* nil)))
+(defmethod gamekit:post-initialize ((app block-push))
+  (gamekit:bind-button :escape :pressed
+    (lambda () (gamekit:stop)))
+  (gamekit:bind-button :up :pressed
+    (lambda () (move-player 1 0)))
+  (gamekit:bind-button :left :pressed
+    (lambda () (move-player 0 -1)))
+  (gamekit:bind-button :right :pressed
+    (lambda () (move-player 0 1)))
+  (gamekit:bind-button :down :pressed
+    (lambda () (move-player -1 0))))
+
+(defun move-player(ud lr)
+	(if (collides (+ *player-x* ud) (+ *player-y* lr))
+                   ()
+                   (progn (setf *player-x* (+ ud *player-x*))
+                          (setf *player-y* (+ lr *player-y*)))))
+
+(defun collides (x y)
+  (if (aref *block-map* x y)
+     '(t)
+     ()))
 
 (defun draw-floor (x y)
-  (gamekit:draw-image (gamekit:vec2 (* 64 y) (* 64 x)) :sprite-sheet
+  (gamekit:draw-image (gamekit:vec2 (* 16 y) (* 16 x)) :sprite-sheet
                               :origin *f1*
-                              :width 64
-                              :height 64))
+                              :width 16
+                              :height 16))
 
 
 (defun draw-block (block-type x y)
   (if block-type
-    (gamekit:draw-image (gamekit:vec2 (* 64 y) (* 64 x)) :sprite-sheet
+    (gamekit:draw-image (gamekit:vec2 (* 16 y) (* 16 x)) :sprite-sheet
                             :origin block-type
-                            :width 64
-                            :height 64)
+                            :width 16
+                            :height 16)
     (draw-floor x y)))
 
 (defun draw-map ()
     (loop 
-      for x from 0 to 10
-      for row in *block-map* do
+      for x from 0 to 8 do
       (loop 
-	 for y from 0 to 10
-         for block-type in row do 
-            (draw-block block-type x y))))
+	 for y from 0 to 8 do
+            (draw-block (aref *block-map* x y) x y))))
 
 (defun draw-player(x y)
-  (gamekit:draw-image (gamekit:vec2 (* 64 y) (* 64 x)) :sprite-sheet
+  (gamekit:draw-image (gamekit:vec2 (* 16 y) (* 16 x)) :sprite-sheet
                               :origin *player-sprite*
-                              :width 64
-                              :height 64))
+                              :width 16
+                              :height 16))
 
 (defmethod gamekit:draw ((app block-push))
+  (gamekit:scale-canvas 4 4)
   (draw-map)
   (draw-player *player-x* *player-y*))
 
